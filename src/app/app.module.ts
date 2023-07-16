bb7026b0464259ec43ad62323368f1bf6b226327
Manisankar Ramachandran <manisankar.ramachandran@kongsbergdigital.com>
2023-07-16 11:26:22 +0530

    employee added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule
